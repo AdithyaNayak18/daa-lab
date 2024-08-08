f11f21b397ec9e59019cda37b4881baf673b811e
Adithya Nayak <153359668+AdithyaNayak18@users.noreply.github.com>
2024-08-08 22:34:32 +0530

    Update toposort.cpp

--- toposort.cpp
+++ toposort.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 class Solution {
 private:
-	void dfs(int node, int vis[], stack<int> &st,
-	         vector<int> adj[]) {
+	void dfs(int node, int vis[], stack<int> &st,vector<int> adj[]) {
 		vis[node] = 1;
 		for (auto it : adj[node]) {
 			if (!vis[it]) dfs(it, vis, st, adj);
